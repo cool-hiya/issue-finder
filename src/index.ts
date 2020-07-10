aee6956f39a0aa5a084086838b3a1cf2776c442f
Arina Busygina <thr3@mail.ru>
2020-07-10 18:45:55 +0300

    Move application start to core folder

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
 import './styles.scss';
-import { Application } from 'stimulus'
-
-import SearchFormController from './components/search-form/search-form-controller';
-import IssueListController from './components/issue-list/issue-list-controller';
-import ErrorController from './components/error/error-controller';
-
-import SearchPageController from './pages/search-page/search-page-controller';
-
-const application = Application.start();
-
-application.register("search-form", SearchFormController);
-application.register("search-page", SearchPageController);
-application.register("issue-list", IssueListController);
-application.register("error", ErrorController);
+import './assets/core/controller-setup';
