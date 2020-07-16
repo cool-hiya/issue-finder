761e60fb6aab1a6b23f96eb742df0c4528070f8d
Arina Busygina <thr3@mail.ru>
2020-07-16 16:05:52 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -5613,9 +5613,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
   !*** ./styles.scss ***!
   \*********************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./styles.scss?");
+eval("throw new Error(\"Module build failed (from ../node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ../node_modules/sass-loader/dist/cjs.js):\\nSassError: Undefined variable: \\\"$gray\\\".\\n        on line 41 of src/app/components/search-form/search-form.scss\\n        from line 8 of C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\src\\\\styles.scss\\n>>             border-bottom: 1px solid $gray;\\r\\n   -------------------------------------^\\n\\n    at runLoaders (C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:316:20)\\n    at C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:367:11\\n    at C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:233:18\\n    at context.callback (C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:111:13)\\n    at Object.render [as callback] (C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\sass-loader\\\\dist\\\\index.js:73:7)\\n    at Object.done [as callback] (C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\neo-async\\\\async.js:8067:18)\\n    at options.error (C:\\\\Users\\\\Arina\\\\Desktop\\\\issue-finder\\\\node_modules\\\\node-sass\\\\lib\\\\index.js:294:32)\");\n\n//# sourceURL=webpack:///./styles.scss?");
 
 /***/ }),
 
